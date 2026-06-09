04fd9f3ca9938b480c3117674d47b454cedaa073
Adriano Dias <dias.adriano.alves@gmail.com>
2026-06-09 18:55:50 -0300

    fix: build front vox

--- modules/vox-observatory/web/next.config.mjs
+++ modules/vox-observatory/web/next.config.mjs
@@ -7,6 +7,9 @@ const __dirname = path.dirname(__filename);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   outputFileTracingRoot: path.join(__dirname, "../../"),
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     externalDir: true,
   },
